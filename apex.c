9cc7c508bd97a45a424f139e92194ee8916a362f
Kelvin Patino <kelvinpatino@gmail.com>
2025-07-22 01:59:07 -0500

    Fourth  commit

--- apex.c
+++ apex.c
@@ -15,9 +15,37 @@ void parseCommandLine(int argc, char *argv[]){
 		printf("argv[%d]=%s\n",i,argv[i]);
 	}
 
+	//if the only commandline argument is the program itself, 
+	//print the menu to help the user out
 	if (argc == 1){
 		menu();
 	}
+
+	//else, if there are multiple command line arguments, iterate through them to determine 
+	//the next course of action
+	
+	// count = 1 so that we start parsing from argument 1, because argument 0 is the program name
+	cnt = 1;
+
+	// loop through all the command line arguments
+	while(cnt < argc){
+		//if current argument is "-c", we can expect a cover file next
+		if(strcasecmp((argv[cnt], "-c") == 0){ 
+				//move to the file name argument
+				cnt ++;
+				//if no file name is provided, that's an error
+				if (cnt == argc){
+				fprintf(stderr, "\n\nError - no file name following <%s> parameter.\n\n", argv[cnt -1]);
+				exit(-1);
+				}
+			//this is where me and isaac stopped, this section is to be finished tomorrow. 
+				
+				
+
+				
+
+
+	}	
 }
 
 //main function with command line argument count and array of command line arguments
