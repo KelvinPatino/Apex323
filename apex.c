e8fabe6d5e5c7a9758206d6a1bf2633cbc0c9318
LisanAlGaib <158761588+SirIsaaac@users.noreply.github.com>
2025-07-22 02:14:31 -0500

    combined Menu file with main apex file

--- apex.c
+++ apex.c
@@ -3,10 +3,34 @@
 #include <string.h>
 #define MAX_PATH = 50
 
-void menu(){
-	printf("\nSince you didn't run the program with arguments, here's the menu to help you out:)\n");
-	//menu function placeholder for isaac to complete
+void Menu(char *program){
+printf("\nSince you didn't run the program with arguments, here's the menu to help you out:)\n");
+
+int choice;
+
+fprintf(stdout, "Program intentions: read a cover image and secret message\n");
+fprintf(stdout, "embed data in a 24-bit image using differential value encoding\n");
+fprintf(stdout, "resulting pixel value applies secret message, then  allows for extraction to process\n");
+fprintf(stdout, "Enter 1 to get information or 9 to exit: ");
+scanf("%d", &choice);
+
+if(choice == 1){
+fprintf(stdout, "To Hide:\n");
+fprintf(stdout, "\t%s -hide -c <cover file> -m<msg file> [-s <stego file>] [-b <number of bits>]\n\n", program);
+
+fprintf(stdout, "To Extract:\n");
+fprintf(stdout, "\t%s -extract -s <cove file> [-m <message file>] [-b <number of bits>]\n\n", program);
+
+fprintf(stdout, "Parameters:\n");
+fprintf(stdout, "\tSet number of bits to hide per pixel:	-b ( 1 to 8 def:1 )\n\n");
+
 }
+if(choice == 9){
+
+exit(0);
+		}
+	}
+
 
 void parseCommandLine(int argc, char *argv[]){
 
@@ -18,7 +42,7 @@ void parseCommandLine(int argc, char *argv[]){
 	//if the only commandline argument is the program itself, 
 	//print the menu to help the user out
 	if (argc == 1){
-		menu();
+		printf("%p",(void*)*Menu);
 	}
 
 	//else, if there are multiple command line arguments, iterate through them to determine 
@@ -52,8 +76,10 @@ void parseCommandLine(int argc, char *argv[]){
 //argc is the number of arguments and *argv[] is the array of arguments themselves
 int main(int argc, char *argv[]) {
 	printf("Hello World\n");
-
+	
 	parseCommandLine(argc, argv);{
+	char program[] = "Menu.c";
+	Menu(program);
 	return 0;
 
 }
