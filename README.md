339d3a34933f2b6295286a094d03eb595dbe58a1
Kelvin Patino <kelvinpatino@gmail.com>
2025-07-22 00:38:14 -0500

    Second commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Apex323
+# This is our project to hide and extract messages from 24 bit bitmap images. 
+# Wish us luck:)
---
+++ apex.c
@@ -0,0 +1,38 @@
+#include<stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+void menu(){
+	printf("\nSince you didn't run the program with arguments, here's the menu to help you out:)\n");
+	//menu function placeholder for isaac to complete
+}
+
+void parseCommandLine(int argc, char *argv[]){
+
+	//programatically print each argument in the command line
+	for (int i = 0; i < argc; i++){
+		printf("argv[%d]=%s\n",i,argv[i]);
+	}
+
+	if (argc == 1){
+		menu();
+	}
+}
+
+
+//main function with command line argument count and array of command line arguments
+//argc is the number of arguments and *argv[] is the array of arguments themselves
+int main(int argc, char *argv[]) {
+	printf("Hello World\n");
+
+	parseCommandLine(argc, argv);{
+	return 0;
+
+}
+
+
+
+
+
+
+}
